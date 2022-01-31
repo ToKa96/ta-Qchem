a22d70444b85926368a79ba41c4f88dcb2e27732
Tobias Kaczun <tobias.kaczun@iwr.uni-heidelberg.de>
2022-01-31 15:06:08 +0100

    ta_util first convergence implemented

--- src/qextract/ta_util.py
+++ src/qextract/ta_util.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+import enum
 import h5py
 import traceback
 import numpy as np
@@ -25,34 +27,78 @@ def lorentzian(x, exc, osc, std_devi=0.4):
         return osc / (1 + np.power((x - exc) / (std_devi/2), 2))
 
 
-def getMesh(filepath, wavelength_arr, time_arr, **kwargs):
+# def getMesh(filepath, wavelength_arr, time_arr, **kwargs):
 
-    kwargs = kwargs
+#     kwargs = kwargs
 
-    return ConstructMesh(wavelength_arr, time_arr, **kwargs)._getMesh(filepath).z
-
-
-def getTA(filepath, wavelength_arr, time_arr, **kwargs):
-
-    kwargs = kwargs    
-
-    return ConstructMesh(wavelength_arr, time_arr, **kwargs)._getMesh(filepath)
-
-def getTAbyStruct(filepath, wavelength_arr, time_arr, structurname=None, **kwargs):
-
-    kwargs = kwargs    
-
-    return ConstructMesh(wavelength_arr, time_arr, **kwargs)._getMesh(filepath, structurname=structurname)
+#     return ConstructMesh(wavelength_arr, time_arr, **kwargs)._getMesh(filepath).z
 
 
+# def getTA(filepath, wavelength_arr, time_arr, **kwargs):
+
+#     kwargs = kwargs    
+
+#     return ConstructMesh(wavelength_arr, time_arr, **kwargs)._getMesh(filepath)
+
+# def getTAbyStruct(filepath, wavelength_arr, time_arr, structurname=None, **kwargs):
+
+#     kwargs = kwargs    
+
+#     return ConstructMesh(wavelength_arr, time_arr, **kwargs)._getMesh(filepath, structurname=structurname)
+
+def norm_max(array):
+    return array / array.max()
+
+def norm_sum(array):
+    return array /array.sum()
 class TA():
 
-    def __init__(self, x, y, z, x_unit=None, y_unit=None, z_unit=None) -> None:
+    def __init__(self, x, y, z, tensor, x_unit=None, y_unit=None, z_unit=None) -> None:
+        self.tensor = tensor
+        self.ta = np.sum(tensor, axis=2)
         self.z = z
         self.x = x
-        self.x_unit = x_unit
         self.y = y
+        self.x_unit = x_unit
         self.y_unit = y_unit
+        self.z_unit = z_unit
+        
+    def convergence(self, criterion, norm=norm_max):
+        if norm == 'max':
+            norm=norm_max
+        if norm == 'sum':
+            norm=norm_sum
+        ta = norm(self.tensor[...,0])
+        for i in range(2, self.tensor.shape[-1]):
+            # TODO: Normalization of arrays still mising, convergence can not be achieved 
+            #       this way
+            tmp = np.copy(ta)
+            ta = norm(self.tensor[...,:i].sum(axis=-1))
+            mse = (np.square(ta-tmp)).mean()
+            if mse < criterion:
+                return ta, mse, i
+        # this should be deleted at some point:
+        print('convergence criterion not met!')
+        return ta, mse, i
+    
+    def convergence_maxSE(self, criterion, norm="max"):
+        if norm == 'max':
+            norm=norm_max
+        if norm == 'sum':
+            norm=norm_sum
+        
+        ta = norm(self.tensor[...,0])
+        for i in range(2, self.tensor.shape[-1]):
+            # TODO: Normalization of arrays still mising, convergence can not be achieved 
+            #       this way
+            tmp = np.copy(ta)
+            ta = norm(self.tensor[...,:i].sum(axis=-1))
+            mse = (np.square(ta-tmp)).max()
+            if mse < criterion:
+                return ta, mse, i
+        # this should be deleted at some point:
+        print('convergence criterion not met!')
+        return ta, mse, i
 
     def getIndexValue(self, arr, value):
         res = np.where(arr == value)[0]
@@ -96,14 +142,52 @@ class TA():
     # def _loc2(self, wvl, time):
 
 
-class ConstructMesh():
+class GetTA():
 
-    def __init__(self, wavelength_arr, time_arr, diabatic=False, std_devi=0.4) -> None:
+    def __init__(self, filepath, wavelength_arr, time_arr=None, diabatic=False, std_devi=0.4, trajectories=None, trajectory_mode='strict') -> None:
         self.std_devi = 0.4
         self.wavelength_arr = wavelength_arr
-        self.time_arr = time_arr
         self.diabatic = diabatic
 
+        with h5py.File(filepath, 'r') as hdf5File:
+            self.hdf5File = hdf5File
+            
+            if time_arr is not None:
+                if isinstance(time_arr, np.ndarray):
+                    self.time_arr = np.asarray(['{:.1f}'.format(x) for x in time_arr])
+                if isinstance(time_arr, list):
+                    self.time_arr = np.asarray(['{:.1f}'.format(x) for x in time_arr])
+            else:
+                self.time_arr = self._getTime(hdf5File)
+
+            if trajectories is not None:
+                self.trajectories=trajectories
+            else:
+                self.trajectories = self._getTrajectories(hdf5File, trajectory_mode=trajectory_mode)
+            
+            self.ta = self._getTA()
+            
+    def _getTrajectories(self, hdf5File, trajectory_mode='strict'):
+        
+        if trajectory_mode == 'strict':
+            return self._getTrajectoriesStrict(hdf5File)
+    
+    def _getTrajectoriesStrict(self, hdf5File):
+        
+        # test = str(self.time_arr[0])
+        tmp_arr = np.fromiter(hdf5File[str(self.time_arr[0])].keys(), dtype='U32')
+        
+        for time in self.time_arr[1:]:
+            curr_arr = np.fromiter(hdf5File[time].keys(),dtype='U32')
+            tmp_arr = np.intersect1d(tmp_arr, curr_arr)
+            
+        return tmp_arr
+        
+    def _getTime(self, hdf5File):
+        sorted_arr = np.fromiter(hdf5File.keys(), dtype=np.float16)
+        sorted_arr = np.sort(sorted_arr)
+        return sorted_arr.astype(str)
+    
     def _calcStateSpectra(self, exc_arr, osc_arr):
 
         y = np.zeros_like(self.wavelength_arr)
@@ -129,64 +213,100 @@ class ConstructMesh():
             if pop > 0:
                 # S1 (i = 1) is mapped on 2_(1)_XX as ground state is 1_(1)_XX therefore i+1
                 pumpName = '{}_(1)_A'.format(i+k)
-                y += pop * self._calcStateSpectra(
-                    structurGroup[pumpName + '/exc_energy'], structurGroup[pumpName + '/osc_strength'])
-
+                try:
+                    y += pop * self._calcStateSpectra(
+                        structurGroup[pumpName + '/exc_energy'], structurGroup[pumpName + '/osc_strength'])
+                except KeyError:
+                    print('{} @ {}'.format(structurGroup.name, pumpName))
         return y
+    
+    def _getTrajectorySpectra(self, trajectory):
+        mesh = np.zeros((self.time_arr.shape[0], self.wavelength_arr.shape[0]))
+        
+        for t, time in enumerate(self.time_arr):
+            mesh[t,:] = self._getStructurSpectra(self.hdf5File[time][trajectory])
+            
+        return mesh
+    
+    def _getAllTrajectories(self):
+        tensor = np.zeros((self.time_arr.shape[0], self.wavelength_arr.shape[0], self.trajectories.shape[0]))
+        
+        for i, trajectory in enumerate(self.trajectories):
+            tensor[:,:,i] = self._getTrajectorySpectra(trajectory)
+            
+        return tensor
+    
+    def _getTA(self):
+        tensor = self._getAllTrajectories()
+        
+        return TA(self.time_arr, self.wavelength_arr, self.trajectories, tensor)
 
-    def _getTimeSpectra(self, timeGroup, structurname=None, **kwargs):
+    # def _getTimeSpectra(self, timeGroup, structurname=None, **kwargs):
 
-        y = np.zeros_like(self.wavelength_arr)
+    #     y = np.zeros_like(self.wavelength_arr)
 
-        # FIXME: should attributes at the structurlevel be added this will make problems!
-        for structur in timeGroup.keys():
-            if structurname:
-                if structur==structurname:
-                    y += self._getStructurSpectra(timeGroup[structur])    
-            else:
-                y += self._getStructurSpectra(timeGroup[structur])
+    #     # FIXME: should attributes at the structurlevel be added this will make problems!
+    #     for structur in timeGroup.keys():
+    #         if structurname:
+    #             if structur==structurname:
+    #                 y += self._getStructurSpectra(timeGroup[structur])    
+    #         else:
+    #             y += self._getStructurSpectra(timeGroup[structur])
 
-        return y
+    #     return y
 
-    def _buildMesh(self, hdf5File, **kwargs):
+    # def _buildMesh(self, hdf5File, **kwargs):
 
-        mesh = np.zeros(
-            (self.time_arr.shape[0], self.wavelength_arr.shape[0]), dtype=np.float64)
+    #     mesh = np.zeros(
+    #         (self.time_arr.shape[0], self.wavelength_arr.shape[0]), dtype=np.float64)
 
-        time_str_list = np.asarray(list(hdf5File.keys()))
-        time_fl_list = np.asarray([float(i) for i in time_str_list])
+    #     time_str_list = np.asarray(list(hdf5File.keys()))
+    #     time_fl_list = np.asarray([float(i) for i in time_str_list])
 
-        for i, time in enumerate(self.time_arr):
+    #     for i, time in enumerate(self.time_arr):
 
-            ind = np.where(time_fl_list == time)
-            time_str = time_str_list[ind][0]
-            try:
-                mesh[i] = self._getTimeSpectra(hdf5File[time_str], **kwargs)
-            except KeyError:
-                # print(traceback.format_exc())
-                print('{time} not found!'.format(time=time))
+    #         ind = np.where(time_fl_list == time)
+    #         time_str = time_str_list[ind][0]
+    #         try:
+    #             mesh[i] = self._getTimeSpectra(hdf5File[time_str], **kwargs)
+    #         except KeyError:
+    #             # print(traceback.format_exc())
+    #             print('{time} not found!'.format(time=time))
 
-        return TA(self.wavelength_arr, self.time_arr, mesh)
+    #     return TA(self.wavelength_arr, self.time_arr, mesh)
 
-    def _getMesh(self, filepath, **kwargs):
+    # def _getMesh(self, filepath, **kwargs):
 
-        kwargs = kwargs
+    #     kwargs = kwargs
 
-        with h5py.File(filepath, 'r') as hdf5File:
-            return self._buildMesh(hdf5File, **kwargs)
+    #     with h5py.File(filepath, 'r') as hdf5File:
+    #         return self._buildMesh(hdf5File, **kwargs)
 
 
 if __name__ == "__main__":
-    hdf5_path = '/export/home/tkaczun/xray-ta/Pyrazine/ta_data.hdf5'
-    time_arr = np.arange(59, 75.5, 0.5)
-    wavelength_arr = np.linspace(280, 286, 50)
+    hdf5_path = '/export/home/tkaczun/xray-ta/Pyrazine/ta_TRAJ10.hdf5'
+    time_arr = np.arange(0, 200, 2)
+    wavelength_arr = np.linspace(280, 290, 100)
 
-    getMesh(hdf5_path, wavelength_arr, time_arr)
+    ta_data = GetTA(hdf5_path, wavelength_arr, time_arr=time_arr).ta
+    
+    for i in [1e-10, 1e-11, 1e-12]:
+        ta , mse, num = ta_data.convergence(i)
+        print(mse, num)
+    
+    # print(ta_data.tensor.shape)
+    # print(ta_data.z)
+    # print(ta_data.z)
+    # print(ta_data.x)
+    # print(ta_data.y)
 
-    data = getTA(hdf5_path, wavelength_arr, time_arr)
+    # import matplotlib.pyplot as plt
+    
+    # plt.figure()
+    
+    # plt.pcolormesh(ta_data.ta)
+    
+    # plt.savefig('/export/home/tkaczun/scripts/qextract/data/ta_util.plot.png', )
+    
 
-    data.loc(281)
-    data.loc([281])
-    data.loc([281, 284])
-    data.loc([281, 284, 1])
     # getMesh(os.getcwd() + '/' + hdf5_path, wavelength_arr, time_arr)
